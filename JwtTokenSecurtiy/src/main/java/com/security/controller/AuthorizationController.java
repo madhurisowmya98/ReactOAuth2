ea619a40c067a2a6ed6e08a9733c30e0cbaf6357
madhurisowmya98 <114557118+madhurisowmya98@users.noreply.github.com>
2024-05-21 12:24:12 +0530

    Update AuthorizationController.java

--- JwtTokenSecurtiy/src/main/java/com/security/controller/AuthorizationController.java
+++ JwtTokenSecurtiy/src/main/java/com/security/controller/AuthorizationController.java
@@ -23,22 +23,35 @@ public class AuthorizationController {
 	GitHubOAuthService gitHubOAuthService;
 
 	@PostMapping("/exchangeCodeForAccessToken")
-	public String exchangeCodeForAccessToken(@RequestBody Map<String, String> codeMap) {
+	public User exchangeCodeForAccessToken(@RequestBody Map<String, String> codeMap) {
 		String authorizationCode = codeMap.get("code");
 		System.out.println("Authorization code received." + authorizationCode);
 
 		// Exchange the authorization code for an access token
 		String accessToken= gitHubOAuthService.exchangeCodeForAccessToken(authorizationCode);
-//		System.out.println(">>>>>>>"+accessToken +"nnnnnnnnn");
+//		System.out.println(">>>>>>>"+accessToken +">>>>>>>");
 		
 		// Use the access token to fetch user details from GitHub's user API
 		GitHubUserDetails userDetails = gitHubOAuthService.fetchUserDetails(accessToken);
 		System.out.println("userDetails "+userDetails);
 		
 //		System.out.println(userDetails);
+		User details = new User();
 
+		details.setName(userDetails.getName());
+		details.setUserName(userDetails.getLogin());
+		details.setPassword(String.valueOf(userDetails.getId()));
+		
+		
+		Set<String> userrole = new HashSet<>();
+		userrole.add("user");
+
+		details.setRoles(userrole);
+
+		User pr = loginservices.loginsave(details);
+		System.out.println("save operation"+pr);
 		// You can now use the userDetails object to access the user's GitHub details.
-		return accessToken;
+		return pr;
 	}
 	
 
